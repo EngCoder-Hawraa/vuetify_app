a07e2f54718780a565a9abd679b1a2ebfa9caa05
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-09 10:49:05 +0300

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="text font-weight-light">This is homepage</h1>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="6" style="border: 1px solid">
+          <v-row>
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+              xl="3"
+              xxl="3"
+              style="border: 1px solid"
+              >Test One</v-col
+            >
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+              xl="3"
+              xxl="3"
+              style="border: 1px solid"
+              >Test Two</v-col
+            >
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+              xl="3"
+              xxl="3"
+              style="border: 1px solid"
+              >Test Three</v-col
+            >
+            <v-col
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+              xl="3"
+              xxl="3"
+              style="border: 1px solid"
+              >Test Four</v-col
+            >
+          </v-row>
+        </v-col>
+        <v-col cols="6" style="border: 1px solid">Arkan</v-col>
+      </v-row>
+    </v-container>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
