6cee26f4200eb13a4f9dfee477daf7283bea9e56
Hawraa Arkan <h.a.j.engineer@gmail.com>
2024-12-30 14:38:11 +0300

    updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -148,7 +148,7 @@
     </v-container>
     <!-- Dialog -->
     <v-btn @click="dialog = true">Click Me</v-btn>
-    <v-dialog v-model="dialog" transition="dialog-bottom-transition">
+    <v-dialog v-model="dialog" persistent>
       <v-card>
         <v-card-title>This is Dialog</v-card-title>
         <v-card-text
@@ -162,39 +162,69 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <!-- List & List Items -->
+    <v-card width="400" class="mx-auto mt-5 mb-5" variant="outlined">
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :value="item.value"
+          active-color="primary"
+        >
+          <v-list-item-title>Title One</v-list-item-title>
+          <template v-slot:prepend>
+            <v-icon>mdi-home</v-icon>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-card>
   </div>
 </template>
 
-<!-- Options API -->
-<!--<script>-->
-<!--export default {-->
-<!--  name: "HomeView",-->
-<!--  data: () => ({-->
-<!--    loading: false,-->
-<!--    dialog: false,-->
-<!--  }),-->
-<!--  methods: {-->
-<!--    clickMe() {-->
-<!--      this.loading = true;-->
-<!--      setTimeout(() => {-->
-<!--        this.loading = false;-->
-<!--      }, 2000);-->
-<!--    },-->
-<!--  },-->
-<!--};-->
-<!--</script>-->
+Options API
+<script>
+export default {
+  name: "HomeView",
+  data: () => ({
+    loading: false,
+    dialog: false,
+    items: [
+      {
+        title: "Test One",
+        value: 1,
+      },
+      {
+        title: "Test Two",
+        value: 2,
+      },
+      {
+        title: "Test Three",
+        value: 3,
+      },
+    ],
+  }),
+  methods: {
+    clickMe() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    },
+  },
+};
+</script>
 
 <!-- Composition API -->
-<script setup>
-import { ref } from "vue";
+<!--<script setup>-->
+<!--import { ref } from "vue";-->
 
-const loading = ref(false);
-const dialog = ref(false);
+<!--const loading = ref(false);-->
+<!--const dialog = ref(false);-->
 
-function clickMe() {
-  loading.value = true;
-  setTimeout(() => {
-    loading.value = false;
-  }, 2000);
-}
-</script>
+<!--function clickMe() {-->
+<!--  loading.value = true;-->
+<!--  setTimeout(() => {-->
+<!--    loading.value = false;-->
+<!--  }, 2000);-->
+<!--}-->
+<!--</script>-->
