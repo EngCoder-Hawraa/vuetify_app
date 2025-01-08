05ab262c21bd418aa4e1ee13c8ba75b1aea803d2
Hawraa Arkan <h.a.j.engineer@gmail.com>
2025-01-08 13:35:35 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <!--Layout & Appbar-->
   <v-layout>
-    <v-app-bar
-      theme="dark"
-      elevation="0"
-      extended
-      extension-height="10"
-      height="70"
-      image="https://picsum.photos/1920/1080?random"
-      style="border-bottom: 1px solid rgba(129, 129, 129, 0.671)"
-    >
-      <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-      <v-icon color="white" class="mr-4">mdi-magnify</v-icon>
-      <v-icon color="white" class="mr-4">mdi-heart</v-icon>
-      <v-icon color="white" class="mr-4">mdi-dots-vertical</v-icon>
-    </v-app-bar>
-    <v-app-bar elevation="2">
-      <!--BreadCrumbs-->
-      <v-breadcrumbs :items="items">
-        <template v-slot:divider>
-          <v-icon>mdi-chevron-right</v-icon>
-        </template>
-      </v-breadcrumbs>
-    </v-app-bar>
+    <!--    <v-app-bar-->
+    <!--      theme="dark"-->
+    <!--      elevation="0"-->
+    <!--      extended-->
+    <!--      extension-height="10"-->
+    <!--      height="70"-->
+    <!--      image="https://picsum.photos/1920/1080?random"-->
+    <!--      style="border-bottom: 1px solid rgba(129, 129, 129, 0.671)"-->
+    <!--    >-->
+    <!--      <v-app-bar-nav-icon color="white"></v-app-bar-nav-icon>-->
+    <!--      <v-spacer></v-spacer>-->
+    <!--      <v-icon color="white" class="mr-4">mdi-magnify</v-icon>-->
+    <!--      <v-icon color="white" class="mr-4">mdi-heart</v-icon>-->
+    <!--      <v-icon color="white" class="mr-4">mdi-dots-vertical</v-icon>-->
+    <!--    </v-app-bar>-->
+    <!--    <v-app-bar elevation="2">-->
+    <!--      &lt;!&ndash;BreadCrumbs&ndash;&gt;-->
+    <!--      <v-breadcrumbs :items="items">-->
+    <!--        <template v-slot:divider>-->
+    <!--          <v-icon>mdi-chevron-right</v-icon>-->
+    <!--        </template>-->
+    <!--      </v-breadcrumbs>-->
+    <!--    </v-app-bar>-->
     <!--Bottom Navigation-->
     <v-bottom-navigation class="bg-red">
       <v-btn><v-icon>mdi-home</v-icon></v-btn>
@@ -31,27 +31,43 @@
       <v-btn><v-icon>mdi-music</v-icon></v-btn>
     </v-bottom-navigation>
     <!--System Bar-->
-    <v-system-bar color="red" elevation="10">
-      <v-icon class="mr-2">mdi-wifi-strength-3</v-icon>
-      <v-icon class="mr-2">mdi-signal</v-icon>
-      <v-icon class="mr-2">mdi-battery</v-icon>
-      <span>08:00 AM</span>
-    </v-system-bar>
+    <!--    <v-system-bar color="red" elevation="10">-->
+    <!--      <v-icon class="mr-2">mdi-wifi-strength-3</v-icon>-->
+    <!--      <v-icon class="mr-2">mdi-signal</v-icon>-->
+    <!--      <v-icon class="mr-2">mdi-battery</v-icon>-->
+    <!--      <span>08:00 AM</span>-->
+    <!--    </v-system-bar>-->
     <!--Footer-->
-    <v-footer class="flex-column" elevation="20" :app="true">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, at
-        doloribus ex illum in laboriosam magnam minima perferendis quod tempora!
-        Ea excepturi fugiat labore velit. Doloribus ex laboriosam officiis
-        temporibus.
-      </p>
-      <div>
-        <v-icon>mdi-home</v-icon>
-        <v-icon>mdi-twitter</v-icon>
-        <v-icon>mdi-facebook</v-icon>
-        <v-icon>mdi-instagram</v-icon>
-      </div>
-    </v-footer>
+    <!--    <v-footer class="flex-column" elevation="20" :app="true">-->
+    <!--      <p>-->
+    <!--        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, at-->
+    <!--        doloribus ex illum in laboriosam magnam minima perferendis quod tempora!-->
+    <!--        Ea excepturi fugiat labore velit. Doloribus ex laboriosam officiis-->
+    <!--        temporibus.-->
+    <!--      </p>-->
+    <!--      <div>-->
+    <!--        <v-icon>mdi-home</v-icon>-->
+    <!--        <v-icon>mdi-twitter</v-icon>-->
+    <!--        <v-icon>mdi-facebook</v-icon>-->
+    <!--        <v-icon>mdi-instagram</v-icon>-->
+    <!--      </div>-->
+    <!--    </v-footer>-->
+    <!--Tabs-->
+    <v-card class="w-100">
+      <v-tabs v-model="tab" stacked>
+        <v-tab v-for="num in 4" :key="num" :value="num">
+          <v-icon>mdi-home</v-icon>
+          Tab {{ num }}
+        </v-tab>
+      </v-tabs>
+      <v-window v-model="tab" class="mt-5">
+        <v-window-item v-for="num in 4" :key="num" :value="num">
+          <v-card>
+            <v-card-title>Card {{ num }}</v-card-title>
+          </v-card>
+        </v-window-item>
+      </v-window>
+    </v-card>
   </v-layout>
 
   <!--Toolbar-->
@@ -70,17 +86,10 @@
   <router-view />
 </template>
 
-<v-layout>
-  <h1></h1>
-</v-layout>
-
-
-
-
-
 <script setup>
 import { ref } from "vue";
-const items = ref(["test One", "test Two", "test Three"]);
+// const items = ref(["test One", "test Two", "test Three"]);
+const tab = ref("");
 </script>
 <style lang="scss">
 #app {
